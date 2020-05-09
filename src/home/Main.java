172503d80ce17e352fc1c86f07fde43f3053b3bf
Leonard Rizta Anugerah Perdana <leonardrizta3@gmail.com>
2020-05-09 09:28:04 +0700

    Add Feature dynamic ui load

--- src/home/Main.java
+++ src/home/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package home;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        primaryStage.setTitle("Sistem Pelayanan Dan Pemesanan Studio Musik");
+        primaryStage.setScene(new Scene(root, 853, 480));
         primaryStage.show();
     }
 
---
+++ src/home/MainController.java
@@ -0,0 +1,51 @@
+package home;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+
+import java.awt.event.MouseEvent;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+    @FXML
+    private BorderPane mainPane;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    @FXML
+    public void changeHomeLayout() {
+        mainPane.setCenter(loadUI("home"));
+    }
+
+    @FXML
+    public void changeOrderLayout() {
+        mainPane.setCenter(loadUI("order"));
+    }
+
+    @FXML
+    public void changeHistoryLayout() {
+        mainPane.setCenter(loadUI("history"));
+    }
+
+    private HBox loadUI(String filename){
+        HBox a = null;
+        try {
+            a = FXMLLoader.load(getClass().getResource(filename+".fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return a;
+    }
+}
