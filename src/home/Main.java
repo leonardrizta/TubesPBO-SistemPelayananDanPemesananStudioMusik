99f0dd6a08283308c76dc5cc142c7f2c61564b7c
Leonard Rizta Anugerah Perdana <leonardrizta3@gmail.com>
2020-05-09 09:51:21 +0700

    Set Home as Default Layout

--- src/home/Main.java
+++ src/home/Main.java
@@ -4,15 +4,18 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+//        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        BorderPane mainPane = FXMLLoader.load(getClass().getResource("main.fxml"));
+        mainPane.setCenter(FXMLLoader.load(getClass().getResource("home.fxml")));
         primaryStage.setTitle("Sistem Pelayanan Dan Pemesanan Studio Musik");
-        primaryStage.setScene(new Scene(root, 853, 480));
+        primaryStage.setScene(new Scene(mainPane, 853, 480));
         primaryStage.show();
     }
 
