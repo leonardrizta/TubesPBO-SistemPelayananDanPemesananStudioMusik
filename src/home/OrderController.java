b3889482d716e207e9eeb66f4e08c1e72c3a07ad
Leonard Rizta Anugerah Perdana <leonardrizta3@gmail.com>
2020-05-13 21:45:33 +0700

    add time to order, and add action when admin click

--- src/home/OrderController.java
+++ src/home/OrderController.java
@@ -4,9 +4,15 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.*;
 
 import java.net.URL;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 public class OrderController implements Initializable {
@@ -16,6 +22,14 @@ public class OrderController implements Initializable {
     private ChoiceBox<String> studioChoiceBox;
     @FXML
     private ChoiceBox<String> durationChoiceBox;
+    @FXML
+    private TextField name;
+    @FXML
+    private Label orderDetail;
+    @FXML
+    private DatePicker orderPlayDate;
+    @FXML
+    private TextField orderPlayTime;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -23,5 +37,25 @@ public class OrderController implements Initializable {
         studioChoiceBox.setValue("Regular");
         durationChoiceBox.setItems(durationChoiceList);
         durationChoiceBox.setValue("1 Jam");
+    }
+
+    @FXML
+    public void checkOrder() {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime localDateTime = LocalDateTime.now();
+        int price;
+        if (studioChoiceBox.getValue().equals("Regular")) {
+            price = 50000;
+        } else {
+            price = 100000;
+        }
+        orderDetail.setText(
+                "Nama Pemesan: \t" + name.getText() + "\n\n" +
+                        "Jenis Studio:\t\t" + studioChoiceBox.getValue() + "\n\n" +
+                        "Durasi Main:\t\t" + durationChoiceBox.getValue() + "\n\n" +
+                        "Biaya: \t\t\t" + price + "\n\n" +
+                        "Tanggal Main:\t\t" + orderPlayDate.getValue() + " " + orderPlayTime.getText() + "\n\n" +
+                        "Tanggal Order:\t" + dateTimeFormatter.format(localDateTime)
+        );
     }
 }
