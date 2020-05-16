3fa16353e52d1d9043cc89c0cc2d95ed5502b726
Deniraz <denidenira007@gmail.com>
2020-05-16 18:14:47 +0700

    hmm

--- src/home/HistoryController.java
+++ src/home/HistoryController.java
@@ -1,25 +1,54 @@
 package home;
 
 import Connectivity.ConnectionClass;
+import javafx.beans.property.StringProperty;
+import javafx.beans.property.StringPropertyBase;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class HistoryController {
     //TODO: connect history to database
+    @FXML
+    private TableView<History> tableDetails;
 
-    private ObservableList<HistoryController>data;
+    @FXML
+    private TableColumn<History, Integer> columnNo;
+
+    @FXML
+    private TableColumn<History, String> columnTanggalPesan;
+
+    @FXML
+    private TableColumn<History, String> columnPemesan;
+
+    @FXML
+    private TableColumn<History, String> columnStudioId;
+
+    @FXML
+    private TableColumn<History, String> columnTanggalMain;
+
+    @FXML
+    private TableColumn<History, String> columnDurasi;
+
+    @FXML
+    private Button btnrefresh;
+
+    private ObservableList<History> data;
     private ConnectionClass dc;
 
-    @Override
     public void initialize(URL url, ResourceBundle rb){
         dc = new ConnectionClass();
     }
@@ -32,15 +61,21 @@ public class HistoryController {
             //execute query and store result
             ResultSet rs = conn.createStatement().executeQuery("SELECT * FROM studioorder");
             while (rs.next()){
-                data.add(new)
+                data.add(new History(rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6)));
             }
-
-
-
-
-
         } catch (SQLException ex){
-            Logger.getLogger(HistoryController.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println("Error"+ex);
         }
+
+        columnTanggalPesan.setCellValueFactory(new PropertyValueFactory<>("tanggalPemesanan"));
+        columnPemesan.setCellValueFactory(new PropertyValueFactory<>("nama"));
+        columnStudioId.setCellValueFactory(new PropertyValueFactory<>("idStudio"));
+        columnTanggalMain.setCellValueFactory(new PropertyValueFactory<>("tanggalMain"));
+        columnDurasi.setCellValueFactory(new PropertyValueFactory<>("durasiMain"));
+
+        tableDetails.setItems(null);
+        tableDetails.setItems(data);
+
+
     }
 }
