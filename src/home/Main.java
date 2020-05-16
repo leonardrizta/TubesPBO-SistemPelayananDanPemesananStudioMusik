0ac291db7d8576e384e72a37db207051302dc6a9
Leonard Rizta Anugerah Perdana <leonardrizta3@gmail.com>
2020-05-16 23:56:51 +0700

    Revert "masih ada errornya, dunno dimana dan ini udah bener apa belom"
    This reverts commit 4c8f96a99e0a0f819f7f8653d8a2f7f40eddae2f.

--- src/home/Main.java
+++ src/home/Main.java
@@ -1,20 +1,12 @@
 package home;
 
 import javafx.application.Application;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
-    private final StringProperty tanggalPemesanan;
-    private final StringProperty nama;
-    private final StringProperty idStudio;
-    private final StringProperty tanggalMain;
-    private final StringProperty durasiMain;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -26,70 +18,6 @@ public class Main extends Application {
         primaryStage.show();
         //TODO "Do Backend here"
     }
-
-    public Main(String tanggalPemesanan, String nama, String idStudio, String tanggalMain, String durasiMain){
-        this.tanggalMain = new SimpleStringProperty(tanggalMain);
-        this.nama = new SimpleStringProperty(nama);
-        this.idStudio = new SimpleStringProperty(idStudio);
-        this.tanggalMain = new SimpleStringProperty(tanggalMain);
-        this.durasiMain = new SimpleStringProperty(durasiMain);
-    }
-
-    public String getTanggalPesan(){
-        return tanggalMain.get();
-    }
-
-    public String getNama() {
-        return nama.get();
-    }
-    public String getIdStudio(){
-        return idStudio.get();
-    }
-
-    public String getTanggalMain() {
-        return tanggalMain.get();
-    }
-
-    public String getDurasiMain() {
-        return durasiMain.get();
-    }
-
-    public void setTanggalPemesanan(String value){
-        tanggalPemesanan.set(value);
-    }
-    public void setNama(String value){
-        nama.set(value);
-    }
-    public void setIdStudio(String value){
-        idStudio.set(value);
-    }
-    public void setTanggalMain(String value){
-        tanggalMain.set(value);
-    }
-    public void setDurasiMain(String value){
-        durasiMain.set(value);
-    }
-
-    public StringProperty tanggalPemesananProperty(){
-        return tanggalPemesanan;
-    }
-
-    public StringProperty namaProperty() {
-        return nama;
-    }
-
-    public StringProperty idStudioProperty() {
-        return idStudio;
-    }
-
-    public StringProperty tanggalMainProperty() {
-        return tanggalMain;
-    }
-
-    public StringProperty durasiMainProperty() {
-        return durasiMain;
-    }
-
 
 
     public static void main(String[] args) {
--- src/home/MainController.java
+++ src/home/MainController.java
@@ -43,6 +43,4 @@ public class MainController implements Initializable {
         }
         return a;
     }
-
-
 }
