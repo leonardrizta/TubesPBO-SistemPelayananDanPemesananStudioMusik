bb5aede46f6885447aba9c4372600ee300715e25
Tyaz Gagaman <muhammad.118140077@student.itera.ac.id>
2020-05-17 13:48:57 +0700

    JANGAN DI MERGE

--- src/home/LoginController.java
+++ src/home/LoginController.java
@@ -1,14 +1,12 @@
 package home;
 
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.PasswordField;
 
-import java.awt.*;
-import java.net.PasswordAuthentication;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class LoginController {
-    //TODO: add backend Login here
+public class LoginController implements Initializable {
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
