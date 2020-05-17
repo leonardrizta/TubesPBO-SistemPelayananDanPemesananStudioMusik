f0a1a8cd99d7da9de2e90e47ef5bade97bde205d
Leonard Rizta Anugerah Perdana <leonardrizta3@gmail.com>
2020-05-17 13:34:34 +0700

    add login controller

---
+++ src/home/LoginController.java
@@ -0,0 +1,14 @@
+package home;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.PasswordField;
+
+import java.awt.*;
+import java.net.PasswordAuthentication;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController {
+    //TODO: add backend Login here
+}
